7f652d46b42fe49988b1a722efca4585fd112655
yugmani <yugmani@yahoo.com>
2021-06-11 19:06:05 -0700

    return valid values from a string and an array

--- script.js
+++ script.js
@@ -46,25 +46,61 @@ function isTruthy(value) {
 // console.log('true: ', isTruthy(true)); //true
 // console.log('false:', isTruthy(false)); //false
 
-
 // *************************************
 // turn any value into its truthy or falsy boolean value by flipping it twice:
 // *************************************
 
 let num1;
-console.log(!!num1); //false
+// console.log(!!num1); //false
 
 let num2 = 10;
-console.log(!!num2);    //true  
+// console.log(!!num2);    //true
 
-const name1 = "Tram";
-console.log(!!name1);   //true
+const name1 = 'Tram';
+// console.log(!!name1);   //true
 
-const name2 = "";
-console.log(!!name2);   //false
+const name2 = '';
+// console.log(!!name2);   //false
 
 const nullValue = null;
-console.log(!!nullValue);   //false
+// console.log(!!nullValue);   //false
 
+// *************************************
+// Applying Truthy and Falsy Values
+// *************************************
 
+function parseValid(str) {
+  const newString = str
+    .split(',')
+    .map(item => item.trim())
+    .filter(item => !!item)
+    .join(' ');
 
+  return newString;
+}
+
+let string1 = 'David,Mike,,Tim,,John, 10,, Jonathan';
+
+// console.log(parseValid(string1));
+// David Mike Tim John 10 Jonathan
+
+// // Which can be further simplified as
+// function parseValid(str) {
+//   const newString = str.split(",")
+//   .map(item =>item.trim())
+//   .filter(Boolean)
+//   .join(" ")
+
+//   return newString;
+// }
+
+// *************************************
+// Applying Truthy and Falsy Values on Array of different values
+// *************************************
+
+const array1 = [10, 20, 'raj', 0, [], '', NaN, 3, undefined, 50, null, 89];
+
+const validArray = array1.filter(Boolean);
+
+console.log(validArray);
+// (7) [10, 20, "raj", Array(0), 3, 50, 89]
